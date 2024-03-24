34e4483a855bac9958d6ff88605d4c2268473f6f
Jayden Wong <128007766+NotNacissu@users.noreply.github.com>
2024-03-24 17:46:54 +1300

    Updated projects.html, created projectList,js
    Implemented vue code for the projects.html page so that I can easily add projects to the page.

---
+++ projectList.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        projects: [
+            { id: 1, name: 'Project 1', description: 'Description of Project 1', link: 'project1.html' },
+            { id: 2, name: 'Project 2', description: 'Description of Project 2', link: 'project2.html' },
+            { id: 3, name: 'Project 3', description: 'Description of Project 3', link: 'project3.html' },
+            { id: 4, name: 'Project 4', description: 'Description of Project 4', link: 'project4.html' },
+            { id: 5, name: 'Project 5', description: 'Description of Project 5', link: 'project5.html' },
+            { id: 6, name: 'Project 6', description: 'Description of Project 6', link: 'project6.html' },
+            { id: 7, name: 'Project 7', description: 'Description of Project 7', link: 'project7.html' },
+            { id: 8, name: 'Project 8', description: 'Description of Project 8', link: 'project8.html' },
+            { id: 9, name: 'Project 9', description: 'Description of Project 9', link: 'project9.html' },
+            { id: 10, name: 'Project 10', description: 'Description of Project 10', link: 'project10.html' },
+            // Add more projects as needed
+        ]
+    }
+});
