8b04fd9012b4947f51e98b66551ab4baed0c62c9
Jayden Wong <128007766+NotNacissu@users.noreply.github.com>
2024-03-29 23:21:33 +1300

    Updated project vue, html and css
    made it so that the vue list of projects contains the date and languages used for the project. header now has its own seperate css from the homepage fading in css. Lastly, made it so that project html shows the date. It gets the date from the list, uses the formatdate function in the vue code to make it more readable and puts it in the top right corner of the project box.

--- projectList.js
+++ projectList.js
@@ -2,15 +2,15 @@ new Vue({
     el: '#app',
     data: {
         projects: [
-            { id: 1, name: 'Project 1', description: 'Description of Project 1', link: 'https://www.youtube.com/watch?v=fevGo3H5840' },
-            { id: 2, name: 'Project 2', description: 'Description of Project 2', link: 'project2.html' },
-            { id: 3, name: 'Project 3', description: 'Description of Project 3', link: 'project3.html' },
-            { id: 4, name: 'Project 4', description: 'Description of Project 4', link: 'project4.html' },
-            { id: 5, name: 'Project 5', description: 'Description of Project 5', link: 'project5.html' },
-            { id: 6, name: 'Project 6', description: 'Description of Project 6', link: 'project6.html' },
-            { id: 7, name: 'Project 7', description: 'Description of Project 7', link: 'project7.html' },
-            { id: 8, name: 'Project 8', description: 'Description of Project 8', link: 'project8.html' },
-            { id: 9, name: 'Project 9', description: 'Description of Project 9', link: 'project9.html' },
+            { id: 1, name: 'Project 1', description: 'Description of Project 1', link: 'https://www.youtube.com/watch?v=fevGo3H5840', languages: ['JavaScript', 'HTML', 'CSS'], date: '2024-03-30' },
+            { id: 2, name: 'Project 2', description: 'Description of Project 2', link: 'project2.html', languages: ['Python', 'Django', 'HTML', 'CSS'], date: '2023-12-30' },
+            { id: 3, name: 'Project 3', description: 'Description of Project 3', link: 'project3.html', languages: ['Java', 'Spring', 'JavaScript', 'HTML'], date: '2023-11-30' },
+            { id: 4, name: 'Project 4', description: 'Description of Project 4', link: 'project4.html', languages: ['C++', 'OpenGL'], date: '2023-10-30' },
+            { id: 5, name: 'Project 5', description: 'Description of Project 5', link: 'project5.html', languages: ['JavaScript', 'React', 'Node.js'], date: '2023-09-30' },
+            { id: 6, name: 'Project 6', description: 'Description of Project 6', link: 'project6.html', languages: ['Python', 'Flask', 'HTML', 'CSS'], date: '2022-08-30' },
+            { id: 7, name: 'Project 7', description: 'Description of Project 7', link: 'project7.html', languages: ['JavaScript', 'Vue.js', 'HTML', 'CSS'], date: '2022-07-30' },
+            { id: 8, name: 'Project 8', description: 'Description of Project 8', link: 'project8.html', languages: ['Ruby', 'Rails', 'JavaScript', 'HTML', 'CSS'], date: '2022-05-30' },
+            { id: 9, name: 'Project 9', description: 'Description of Project 9', link: 'project9.html', languages: ['JavaScript', 'Angular', 'HTML', 'CSS'], date: '2022-03-30' }
             // Add more projects as needed
         ]
     },
@@ -22,6 +22,12 @@ new Vue({
     methods: {
         navigateToProject: function(link) {
             window.location.href = link;
+        }
+    },
+    filters: {
+        formatDate: function(value) {
+            const date = new Date(value);
+            return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
         }
     }
 });
